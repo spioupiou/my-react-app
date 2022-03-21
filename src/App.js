8711ad343f20be5d94abe08f2479aee8206ac88e
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-03-21 19:56:20 +0900

    onClick event handling

--- src/App.js
+++ src/App.js
@@ -32,11 +32,15 @@ const list = [
 ];
 
 const App = () => {
+  const handleChange = (event) => {
+    console.log(event.target.value);
+  }
+
   return (
     <div>
       <h1>Hello {getTitle('React')}</h1>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange}/>
       <hr />
       <List />
     </div>
@@ -57,4 +61,5 @@ const List = () => {
     })}
   </ul>;
 }
+
 export default App;
