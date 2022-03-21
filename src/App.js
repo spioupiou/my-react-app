1de3ffa7f1469770e3bdc3f0b14ae976bb195b61
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-03-21 16:39:44 +0900

    List function

--- src/App.js
+++ src/App.js
@@ -3,12 +3,58 @@ import React from 'react';
 const title = "React"
 
 // Function component
+// For is a reserved word in JS, that's why label for becomes label htmlFor
+
+const getTitle = (title) => {
+  return title;
+}
+
+// JS must be inside curly braces
+// const title = 'React' -> <h1>Hello {title}!</h1>
+
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  },
+  {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },
+];
+
 const App = () => {
   return (
     <div>
-      Hello {title}!
+      <h1>Hello {getTitle('React')}</h1>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <hr />
+      <List />
     </div>
   );
 };
 
+const List = () => {
+  return <ul>
+    {list.map((article) => {
+      return (
+        <li key={article.objectID}>
+          <a href={article.url}>
+            {article.title}
+          </a>
+          <span> - {article.author}</span>
+        </li>
+      );
+    })}
+  </ul>;
+}
 export default App;
