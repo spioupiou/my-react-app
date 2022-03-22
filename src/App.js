436ad31428d5dba6fef608cfa960ce0b022dec05
Cedrine Monnet <cedrine.monnet@gmail.com>
2022-03-22 18:13:13 +0900

    First use of props

--- src/App.js
+++ src/App.js
@@ -12,26 +12,27 @@ const getTitle = (title) => {
 // JS must be inside curly braces
 // const title = 'React' -> <h1>Hello {title}!</h1>
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-];
 
 const App = () => {
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+  ];
+
   const handleChange = (event) => {
     console.log(event.target.value);
   }
@@ -42,14 +43,14 @@ const App = () => {
       <label htmlFor="search">Search: </label>
       <input id="search" type="text" onChange={handleChange}/>
       <hr />
-      <List />
+      <List list={stories}/>
     </div>
   );
 };
 
-const List = () => {
+const List = (props) => {
   return <ul>
-    {list.map((article) => {
+    {props.list.map((article) => {
       return (
         <li key={article.objectID}>
           <a href={article.url}>
